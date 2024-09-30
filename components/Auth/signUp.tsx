f6fc08544cd3974c09321e4e72f8ff22300c18b3
Shahanees1998 <shahanees1998@gmail.com>
2024-09-30 16:03:17 +0500

    fix: removed unessar text

--- components/Auth/signUp.tsx
+++ components/Auth/signUp.tsx
@@ -31,6 +31,7 @@ const SignUpComponent = (): JSX.Element => {
                                 </Link>
                             </div>
                             <p className="text-base leading-relaxed text-body-color dark:text-dark-6 text-center text-sm mt-5 mb-5">
+
                             </p>
                             <div className="mb-2 relative">
                                 <label className="text-black text-sm font-medium">Fullständigt namn</label>
